2b8b3159e943c59963ba75962ddd2b0e52ff8bec
ariqirawann <165389522+ariqirawann@users.noreply.github.com>
2025-10-27 20:40:22 +0700

    Update DesktopNavbar.jsx

--- src/components/navbar/DesktopNavbar.jsx
+++ src/components/navbar/DesktopNavbar.jsx
@@ -1,4 +1,3 @@
-// src/components/DesktopNavbar.jsx
 import logoUrl from '../../assets/LOGORN.png';
 
 export default function DesktopNavbar({ currentPage, onNavigate }) {
@@ -15,7 +14,6 @@ export default function DesktopNavbar({ currentPage, onNavigate }) {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           
-          {/* Logo */}
           <div className="flex items-center space-x-4">
             <div className="relative group">
               <img
@@ -23,7 +21,6 @@ export default function DesktopNavbar({ currentPage, onNavigate }) {
                 alt="Resep Nusantara Logo"
                 className="w-12 h-12 object-contain filter drop-shadow-md transform transition-transform duration-300 group-hover:scale-110"
               />
-              {/* Decorative particles */}
               <div className="absolute -top-0.5 -right-0.5 w-1.5 h-1.5 bg-blue-400 rounded-full animate-ping opacity-60" />
               <div className="absolute -bottom-0.5 -left-0.5 w-1 h-1 bg-blue-300 rounded-full animate-ping opacity-50" style={{ animationDelay: '300ms' }} />
             </div>
@@ -37,7 +34,6 @@ export default function DesktopNavbar({ currentPage, onNavigate }) {
             </div>
           </div>
 
-          {/* Navigation Links */}
           <div className="flex items-center space-x-10">
             {navItems.map((item) => (
               <button
@@ -58,4 +54,5 @@ export default function DesktopNavbar({ currentPage, onNavigate }) {
       </div>
     </nav>
   );
+
 }
